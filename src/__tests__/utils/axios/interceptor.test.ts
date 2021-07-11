66f177c8b25ac9c0d6d7581312a000fe4c1aa974
Sumanth Akkala <sumanth.akkala@gmail.com>
2021-07-11 11:27:00 -0400

    feat: :wrench: Added husky pre commit hook to test global coverage to 100

--- src/__tests__/utils/axios/interceptor.test.ts
+++ src/__tests__/utils/axios/interceptor.test.ts
@@ -1,7 +1,6 @@
 import {tmbdAxios} from 'utils/axios/interceptor'
 
 test('interceptor works correctly for TMDB requests', () => {
-
   const result = tmbdAxios.interceptors.request.handlers[0].fulfilled({});
   expect(result.params).toHaveProperty('api_key');
 })
