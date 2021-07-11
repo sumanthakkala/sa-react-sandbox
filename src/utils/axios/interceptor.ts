78b80a8d556dc3f824eba365744810118123b708
Sumanth Akkala <sumanth.akkala@gmail.com>
2021-07-11 12:52:19 -0400

    feat: :sparkles: Added requests utility

--- src/utils/axios/interceptor.ts
+++ src/utils/axios/interceptor.ts
@@ -3,6 +3,7 @@ import axios from 'axios'
 const tmbdAxios = axios.create()
 
 tmbdAxios.interceptors.request.use((request) => {
+  request.baseURL = '' + process.env.REACT_APP_TMDB_BASE_URL;
   request.params = request.params ?? {}
   request.params['api_key'] = process.env.REACT_APP_TMDB_API_KEY
   return request
