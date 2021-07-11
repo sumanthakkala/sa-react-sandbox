f5903e03d7cb8d1ecba07e4b282054bb49e2aabe
Sumanth Akkala <sumanth.akkala@gmail.com>
2021-07-11 10:29:03 -0400

    test: :sparkles: Added test coverage for tmdbInterceptor

---
+++ src/__tests__/utils/axios/interceptor.test.ts
@@ -0,0 +1,7 @@
+import {tmbdAxios} from 'utils/axios/interceptor'
+
+test('interceptor works correctly for TMDB requests', () => {
+
+  const result = tmbdAxios.interceptors.request.handlers[0].fulfilled({});
+  expect(result.params).toHaveProperty('api_key');
+})
--- src/utils/axios/interceptor.ts
+++ src/utils/axios/interceptor.ts
@@ -1,16 +1,11 @@
 import axios from 'axios'
 
-export const setTmdbInterceptor = (): void => {
-  console.log(process.env)
+const tmbdAxios = axios.create()
 
-  axios.interceptors.request.use((request) => {
-    console.log(request)
-    if (
-      request.url?.startsWith(process.env.REACT_APP_TMDB_BASE_URL as string)
-    ) {
-      request.params = request.params ?? {}
-      request.params['api_key'] = process.env.REACT_APP_TMDB_API_KEY
-    }
-    return request
-  })
-}
+tmbdAxios.interceptors.request.use((request) => {
+  request.params = request.params ?? {}
+  request.params['api_key'] = process.env.REACT_APP_TMDB_API_KEY
+  return request
+})
+
+export {tmbdAxios}
